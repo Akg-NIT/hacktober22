79e0452343553abe0334d57d4671d2743373a0f7
PawanGupta14 <115361142+PawanGupta14@users.noreply.github.com>
2022-10-17 16:51:15 +0530

    Hash function
    hacktober fest accept

---
+++ hash_function.c
@@ -0,0 +1,10 @@
+#define FNV_OFFSET 14695981039346656037UL
+#define FNV_PRIME 1099511628211UL
+static uint64_t hash_key(const char* key) {
+    uint64_t hash = FNV_OFFSET;
+    for (const char* p = key; *p; p++) {
+        hash ^= (uint64_t)(unsigned char)(*p);
+        hash *= FNV_PRIME;
+    }
+    return hash;
+}
